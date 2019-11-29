3b5e048c57c170043e318643e68ad9751c856fa0
Vladislav <dsfcoll@gmail.com>
2019-11-29 17:44:27 +0200

    Data dummy modified

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,6 @@
 export default {
   currentClothPaths (state) {
     // Now it 0, later it can be selected from right menu
-    return state.paths[0] && state.paths[0].model
+    return state.paths[0] && state.paths[0].paths
   }
 }
