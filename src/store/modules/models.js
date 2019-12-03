192e692058c176685ad85f279c660ea38a010967
Vladislav <dsfcoll@gmail.com>
2019-12-03 12:14:03 +0200

    Fix the bug then selected model becomes unreachable by change filters

--- src/store/modules/models.js
+++ src/store/modules/models.js
@@ -9,9 +9,8 @@ export default {
     SET_MODEL: (state, model) => { state.curModel = model }
   },
   getters: {
-    currentModel (state) {
-      // Now it 0, later it can be selected from right menu
-      return state.entities.find(entity => entity.id === state.curModel)
+    currentModel (state, getters) {
+      return getters.suitableModels.find(model => model.id === state.curModel)
     },
     suitableModels (state, getters) {
       return state.entities.filter(model => {
